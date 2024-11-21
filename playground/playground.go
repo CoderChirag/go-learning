02a67d30fe1d3f5a92e3c78167f835a334492edc
Chirag <chirag.jain1@bfhl.in>
2024-11-21 23:33:29 +0530

    add playground

---
+++ playground/playground.go
@@ -0,0 +1,16 @@
+package playground
+
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func Playground() {
+	r := strings.NewReader("some random string\n")
+
+	if _, err := io.Copy(os.Stdout, r); err != nil {
+		log.Fatal(err)
+	}
+}
