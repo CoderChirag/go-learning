241fc3cbecfa3d1962cd4564c7a28e3d162949b0
Chirag <chirag.jain1@bfhl.in>
2024-12-18 20:11:05 +0530

    add fmt package

--- playground/playground.go
+++ playground/playground.go
@@ -1,16 +1,15 @@
 package playground
 
 import (
-	"io"
-	"log"
-	"os"
-	"strings"
+	libFmt "github.com/coderchirag/go-learning/stdlib/fmt"
 )
 
 func Playground() {
-	r := strings.NewReader("some random string\n")
+	// r := strings.NewReader("some random string\n")
 
-	if _, err := io.Copy(os.Stdout, r); err != nil {
-		log.Fatal(err)
-	}
+	// if _, err := io.Copy(os.Stdout, r); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	libFmt.FmtExamples()
 }
