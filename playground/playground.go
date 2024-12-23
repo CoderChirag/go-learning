a238250fda33f504d4ce5b5129c50d900b91e584
coderchirag <jain.chirag0174@gmail.com>
2024-12-23 23:13:02 +0530

    add worker pools

--- playground/playground.go
+++ playground/playground.go
@@ -3,6 +3,7 @@ package playground
 import (
 	"github.com/coderchirag/go-learning/fundamentals/enums"
 	"github.com/coderchirag/go-learning/fundamentals/timers"
+	"github.com/coderchirag/go-learning/fundamentals/worker_pools"
 	libErrors "github.com/coderchirag/go-learning/stdlib/errors"
 	libFmt "github.com/coderchirag/go-learning/stdlib/fmt"
 	libIter "github.com/coderchirag/go-learning/stdlib/iter"
@@ -29,10 +30,15 @@ func RunTimersExamples() {
 	timers.TimersExamples()
 }
 
+func RunWorkerPoolsExamples() {
+	worker_pools.WorkerPoolsExample()
+}
+
 func Playground() {
 	// RunFmtExamples()
 	// RunEnumsExamples()
 	// RunIterExamples()
 	// RunErrorsExamples()
-	RunTimersExamples()
+	// RunTimersExamples()
+	RunWorkerPoolsExamples()
 }
